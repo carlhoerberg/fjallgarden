b527859ce89770a8a9cc8255f2106cc23c5fb148
Carl Hörberg <carl@84codes.com>
2025-03-19 21:15:39 +0100

    scripts for radiator shunt

---
+++ shelly-scripts/192.168.40.14/1_Prometheus.js
@@ -0,0 +1,27 @@
+/*
+ * This script exposes a "/status" endpoint that returns Prometheus metrics that can be scraped.
+ * It will be reachable under "<ip>/script/<id>/metrics". Id will be 1 if this is your first script.
+ *
+ * Example Prometheus config:
+ *
+ * scrape_configs:
+ *   - job_name: 'shelly'
+ *     metrics_path: /script/1/metrics
+ *     static_configs:
+ *       - targets: ['<ip>']
+ */
+
+function httpServerHandler(request, response) {
+  response.body = [
+    "# HELP shelly_temperature gauge",
+    "# HELP shelly_cover_position gauge",
+    "shelly_temperature{name=\"Radiatorkrets retur\"} " + Shelly.getComponentStatus("temperature:100").tC,
+    "shelly_temperature{name=\"Radiatorkrets framledning\"} " + Shelly.getComponentStatus("temperature:101").tC,
+    "shelly_temperature{name=\"Radiatorkrets primär\"} " + Shelly.getComponentStatus("temperature:102").tC,
+    "shelly_cover_position{name=\"Radiatorkrets shuntposition\"} " + Shelly.getComponentStatus("cover:0").current_pos
+  ].join("\n")
+  response.headers = [["Content-Type", "text/plain"]]
+  response.send()
+}
+
+HTTPServer.registerEndpoint("metrics", httpServerHandler);
